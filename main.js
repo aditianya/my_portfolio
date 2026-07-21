f0545683c7e7b17e9590c5a3f47c52ab85bc70ff
aditianya <aditi.240101260@iiitbh.ac.in>
2026-07-21 15:27:59 +0530

    Initial portfolio project

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Frontend Developer","AI-ML Enthusiast" , "TnP Coordinator","Mentor" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
