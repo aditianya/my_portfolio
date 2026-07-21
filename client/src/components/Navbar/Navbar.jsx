15e3e1aaea680a2c11305e34967304d82421c367
aditianya <aditi.240101260@iiitbh.ac.in>
2026-07-21 16:10:04 +0530

    navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <h2>Aditi</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
