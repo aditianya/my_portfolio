146bf57ac9fa6b0f4c7dbb581e40c4dbbce5265f
aditianya <aditi.240101260@iiitbh.ac.in>
2026-07-30 23:00:03 +0530

    backend ready for deployment

--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 
-connectDB();
+await connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
