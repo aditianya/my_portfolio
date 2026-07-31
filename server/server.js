5bfc781d38f93b6d22c2b234eb901ba09c328eb4
aditianya <aditi.240101260@iiitbh.ac.in>
2026-07-31 11:51:23 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -6,4 +6,8 @@ dotenv.config();
 
 await connectDB();
 
-export default app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
